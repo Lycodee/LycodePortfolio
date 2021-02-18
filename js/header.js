c4160f7dcc14e651052d9b7274152d60c8837f89
Lycode <decol.kevin@mail.com>
2021-02-18 15:36:37 +0100

    Create folders: -css -images -js Create file: -main.css -favicon.png -header-banner.jpg -profile-img.png -footer.js -header.js -nav.js Finished styling of index.html Finished index.html

---
+++ js/header.js
@@ -0,0 +1,7 @@
+document.write("<div  id='banner'>");
+document.write("    <div id='banner-text'>");
+document.write( '       <h1>Lycode</h1>\n' );
+document.write( '       <p>Je fais du Front-end HTML + CSS + JavaScript.<br>\n' );
+document.write("    </div>");
+document.write("</div>");
+document.write( '<script src=\"js/nav.js\"></script>\n' );
