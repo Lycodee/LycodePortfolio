7ecb65aae2532c253993ced819d2b241c2cf921b
Kevin Decoly <49372757+Lycodee@users.noreply.github.com>
2021-02-27 09:25:24 +0100

    Update nav.js

--- js/nav.js
+++ js/nav.js
@@ -2,5 +2,5 @@ document.write("<nav>");
 document.write("<a href='index.html'>Accueil</a>");
 document.write("<a href='#about'>À propos de moi</a>");
 document.write("<a href='#competence'>Compétences</a>");
-document.write("<a href='project.html'>Projets</a>");
+document.write("<a href='#'>Projets</a>");
 document.write("</nav>");
